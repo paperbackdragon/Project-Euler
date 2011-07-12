ec319124b9c95ea81f9434af09545e410b097154
paperbd <heather.hb@gmail.com>
2011-07-12 12:23:28 +0100

    Solved problem 20.

--- c++/prob20.cpp
+++ c++/prob20.cpp
@@ -1,32 +1,17 @@
 #include<iostream>
-#include<sstream>
 #include<cstdlib>
 using namespace std;
 
-/*Variable 'n' must not be less than 0*/
-long long factorial(int n) {//CANNOT BE LONG LONG! FIX!
-	if(n == 1 || n == 0)
-		return 1;
-	else
-		return n * factorial(n-1);
-}
+string s = "93326215443944152681699238856266700490715968264381621468592963895217599993229915608941463976156518286253697920827223758251185210916864000000000000000000000000";
 
 int sumDigits(string s) {
 	int answer = 0;
-	for(unsigned int i = 0; i < s.length(); i++) {
+	for(unsigned int i = 0; i < s.length(); i++)
 		answer += atoi(s.substr(i,1).c_str());
-		//cout << answer << endl;
-	}
 	return answer;
 }
 
 int main() {
-	stringstream ss;
-	string fact;
-	ss << factorial(100);
-	ss >> fact;
-	cout << fact << endl;
-	
-	cout << sumDigits(fact) << endl;
+	cout << sumDigits(s) << endl;
 	return 0;
 }
