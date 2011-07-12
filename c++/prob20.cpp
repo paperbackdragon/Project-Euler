a9b727d20eadfcd53fb0144597055e312d76bea8
paperbd <heather.hb@gmail.com>
2011-07-12 12:00:15 +0100

    Started problem 20.

--- c++/prob20.cpp
+++ c++/prob20.cpp
@@ -1,7 +1,32 @@
 #include<iostream>
+#include<sstream>
+#include<cstdlib>
 using namespace std;
 
+/*Variable 'n' must not be less than 0*/
+long long factorial(int n) {//CANNOT BE LONG LONG! FIX!
+	if(n == 1 || n == 0)
+		return 1;
+	else
+		return n * factorial(n-1);
+}
+
+int sumDigits(string s) {
+	int answer = 0;
+	for(unsigned int i = 0; i < s.length(); i++) {
+		answer += atoi(s.substr(i,1).c_str());
+		//cout << answer << endl;
+	}
+	return answer;
+}
+
 int main() {
-	//
+	stringstream ss;
+	string fact;
+	ss << factorial(100);
+	ss >> fact;
+	cout << fact << endl;
+	
+	cout << sumDigits(fact) << endl;
 	return 0;
 }
