6a5c459e0f5b6998a43b91859a5250faca6d2f0b
paperbd <heather.hb@gmail.com>
2011-07-14 13:10:52 +0100

    Solved problem 24.

--- c++/prob24.cpp
+++ c++/prob24.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main() {
-	//
+	int nums[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+	for(int i = 1; i < 1000000; i++)
+		next_permutation(nums, nums + 10);
+	
+	for(int i = 0; i < 10; i++)
+		cout << nums[i];
+			
 	return 0;
 }
