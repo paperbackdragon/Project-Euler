3f5ca2d3249967c0886ef4a9ef908b81165a11c4
paperbd <heather.hb@gmail.com>
2011-07-15 17:15:49 +0100

    Coded problem 27 but it isn't solved yet.

---
+++ c++/prob27.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "Helper.h"
+using namespace std;
+
+int formula(int a, int b, int n) {
+	return ((n * n) + (a * n) + b);
+}
+
+int main() {
+	int most = 0;
+	int ansA = 0;
+	int ansB = 0;
+	bool end = false;
+	
+	for(int a = 999; a >= -999; a--) {
+		for(int b = 999; b >= 2; b--) { //b must be prime
+			if(Helper::isPrime(b)) {
+				end = false;
+				for(int n = 0; !end; n++) {
+					if(!Helper::isPrime(formula(a, b, n))) {
+						if(n-1 > most) {
+							most = n;
+							ansA = a;
+							ansB = b;
+						}
+						end = true;
+					}
+				}
+			}
+		}
+	}
+	cout << ansA << " * " << ansB << " = " << ansA * ansB << " with " << most << " primes." << endl;
+	return 0;
+}
