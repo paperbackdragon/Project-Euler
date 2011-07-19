43c046e8d23ac78cd1685189f92f22bceaa300e8
paperbd <heather.hb@gmail.com>
2011-07-19 23:41:14 +0100

    Solved problem 31.

--- c++/prob31.cpp
+++ c++/prob31.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 int coin[] = {1, 2, 5, 10, 20, 50, 100, 200};
 
+/*Amounts entered must be in pence.*/
+int combinations(int amount, int size) {
+	int comboCount = 0;
+	
+	if(amount > 0) {
+		if(size >= 0 && amount >= coin[size])
+			comboCount += combinations(amount - coin[size], size);
+		if(size > 0) //don't do if size is 0
+			comboCount += combinations(amount, size-1);
+	} else if(amount == 0)
+		comboCount++;
+
+	return comboCount;
+}
+
 int combos(int amount) {
-	static combos = 0;
-	//Find largest coin < amount
-	//Steadily find smaller denominations for each coin
-	return combos;
+	int i = 0;
+	//get largest coin that fits
+	for(i = 7; coin[i] > amount && i >= 0; i--); 
+	return combinations(amount, i);
 }
 
 int main() {
-	cout << combos(200) << endl;
+	cout << "Answer: " << combos(200) << endl;
 	return 0;
 }
