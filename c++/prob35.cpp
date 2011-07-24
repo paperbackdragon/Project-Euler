97227fed56bb9b0a5ed177bd229d987a1c139411
Heather Hoaglund-Biron <heather.hb@gmail.com>
2011-07-24 03:39:07 +0100

    Continued problem 35.

--- c++/prob35.cpp
+++ c++/prob35.cpp
@@ -1,8 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include "Helper.h"
 using namespace std;
 
 int main() {
-	//
+    int prime = 0;
+    int counter = 0;
+    vector<int> digits;
+    bool permPrime;
+
+	for(int i = 0; i < 1000000; i++) {
+        if(Helper::isPrime(i)) {
+            //separate digits
+            prime = i;
+            for(; prime > 0; prime /= 10)
+                digits.push_back(prime % 10);
+            //check if circular
+            permPrime = true;
+            while(next_permutation(digits.front(), digits.back())) {
+                //combine digits
+                prime = 0;
+                for(int j = 0; j < digits.size(); j++)
+                    prime = (prime * 10) + digits[j];
+                if(!Helper::isPrime(prime))
+                    permPrime = false;
+            }
+            if(permPrime)
+                counter++;
+
+        }
+    }
+    cout << counter << endl;
 	return 0;
 }
