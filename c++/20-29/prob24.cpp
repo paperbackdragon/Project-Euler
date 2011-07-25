d70853a6275b24f098a560cd331c6593d73d9839
paperbd <heather.hb@gmail.com>
2011-07-25 17:24:22 +0100

    Fixed tabs in problem 24.

--- c++/20-29/prob24.cpp
+++ c++/20-29/prob24.cpp
@@ -8,10 +8,10 @@ Answer: 2783915460 */
 using namespace std;
 
 int main() {
-	int nums[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	for(int i = 1; i < 1000000; i++)
-		next_permutation(nums, nums + 10);
-	for(int i = 0; i < 10; i++)
-		cout << nums[i];
-	return 0;
+    int nums[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for(int i = 1; i < 1000000; i++)
+        next_permutation(nums, nums + 10);
+    for(int i = 0; i < 10; i++)
+        cout << nums[i];
+    return 0;
 }
