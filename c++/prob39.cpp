8e90231d3ccb39065ba431d6ba2953a8587be11a
paperbd <heather.hb@gmail.com>
2011-07-26 17:13:32 +0100

    Added blank problem 39.

---
+++ c++/prob39.cpp
@@ -0,0 +1,13 @@
+/* URL: http://projecteuler.net/index.php?section=problems&id=39
+If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three solutions for p = 120.
+{20,48,52}, {24,45,51}, {30,40,50}
+For which value of p <= 1000 is the number of solutions maximised?
+Answer: ? */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    //
+    return 0;
+}
