0d0bd1b6a808347ddbf32b45128f40975972fcde
paperbd <heather.hb@gmail.com>
2011-07-27 17:55:11 +0100

    Moved problems 30-39 into a folder.

---
+++ c++/30-39/Helper.h
@@ -0,0 +1,39 @@
+#ifndef HELPER_H_
+#define HELPER_H_
+
+class Helper {
+private:
+    Helper() {}
+public:
+    /* Converts given integer to a string */
+    static std::string convertInt(int);
+	
+	/* Returns the number of digits in given integer */
+	static int numDigits(int);
+    
+    /* Concatenates integers in given array
+    NOTE: Second argument is size of array */
+    static int concat(int[], int);
+	
+    /* Computes the factorial of an integer */
+    static int factorial(int);
+
+    /* Reduces given fraction to smallest values
+    NOTE: Fractions must be < 1 */
+    static int* reduce(int*);
+
+    /* Finds the sum of the divisors of an integer */
+    static int sumOfDivs(int);
+
+    /* Returns true if given integer is prime */
+    static bool isPrime(int);
+    
+    /* Returns true if given integer is pandigital
+    NOTE: Does not check for digits used more than once */
+    static bool isPandigital(int);
+	
+	/* Returns true if given string is a palindrome */
+	static bool isPalindrome(std::string);
+};
+
+#endif
