c58511e6842d91c06d4b87c3c896cf4326b4dfce
paperbd <heather.hb@gmail.com>
2011-07-28 14:21:43 +0100

    Added blank problem 41.

---
+++ c++/prob41.cpp
@@ -0,0 +1,13 @@
+/* URL: http://projecteuler.net/index.php?section=problems&id=41
+We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
+What is the largest n-digit pandigital prime that exists?
+Answer: ? */
+
+#include <iostream>
+#include "Helper.h"
+using namespace std;
+
+int main() {
+    //
+    return 0;
+}
