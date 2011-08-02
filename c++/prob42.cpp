5a5377b61840711162294872e55010975dd862c0
paperbd <heather.hb@gmail.com>
2011-08-02 13:55:12 +0100

    Added blank problem 42.

---
+++ c++/prob42.cpp
@@ -0,0 +1,15 @@
+/* URL: http://projecteuler.net/index.php?section=problems&id=42
+The nth term of the sequence of triangle numbers is given by, t_n = (1/2)n(n+1); so the first ten triangle numbers are:
+1, 3, 6, 10, 15, 21, 28, 36, 45, 55, ...
+By converting each letter in a word to a number corresponding to its alphabetical position and adding these values we form a word value. For example, the word value for SKY is 19 + 11 + 25 = 55 = t_10. If the word value is a triangle number then we shall call the word a triangle word.
+Using words.txt (right click and 'Save Link/Target As...'), a 16K text file containing nearly two-thousand common English words, how many are triangle words?
+Answer: ? */
+
+#include <iostream>
+#include "Helper.h"
+using namespace std;
+
+int main() {
+    //
+    return 0;
+}
