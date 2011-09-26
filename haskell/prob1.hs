9064ee0df929343ee6b4268278954d8abe4b00e4
paperbd <heather.hb@gmail.com>
2011-09-26 17:21:35 +0100

    Solved problem 1.

--- haskell/prob1.hs
+++ haskell/prob1.hs
@@ -1,1 +1,4 @@
-main = sum [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
+answer :: Int
+answer = sum [x | x <- [1..999], x `mod` 3 == 0 || x `mod` 5 == 0]
+
+main = putStrLn $ show $ answer
