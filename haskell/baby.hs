ae2294c7dfb494b97dcd46bda45d9c34241cb98c
paperbd <heather.hb@gmail.com>
2011-09-27 10:06:31 +0100

    Added programs/notes from 'Learn You A Haskell'.

---
+++ haskell/baby.hs
@@ -0,0 +1,13 @@
+--Examples from "Learn You A Haskell For Great Good"--
+
+doubleMe x = x + x
+
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100
+						then x
+						else x*2
+						
+doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
+--left off at: "I'm a list comprehension"--
