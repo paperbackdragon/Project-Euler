3acd2371a235bc9d235e700049e0135654d8374a
paperbd <heather.hb@gmail.com>
2012-03-25 21:55:54 +0100

    Continued with problem 3 and slightly edited problem 4.

--- haskell/prob3.hs
+++ haskell/prob3.hs
@@ -1,10 +1,15 @@
+{- The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143? -}
+
+main = putStrLn $ show $ lpFact 600851475143
+
 lpFact :: Integer -> Integer
 lpFact x
-    | not null [y | y <- reverse [2..(round (sqrt (fromIntegral x)))], x `mod` y == 0]
+    | isPrime x = 
 
 isPrime :: Integer -> Bool
 isPrime x
     | x == 2                  = True
     | x < 2 || x `mod` 2 == 0 = False
-    | otherwise = null [y | y <- reverse [2..(round (sqrt (fromIntegral x)))], x `mod` y == 0]
+    | otherwise = null [y | y <- reverse [2..(round $ sqrt $ fromIntegral x)], x `mod` y == 0]
 
