d708abc476b1b054cf92c58d77debee74fdda721
paperbd <heather.hb@gmail.com>
2012-03-25 21:55:17 +0100

    Edited problem 2.

--- haskell/prob2.hs
+++ haskell/prob2.hs
@@ -3,5 +3,5 @@ fib x y zs
     | x + y > 4000000 = zs
     | otherwise = fib y (x+y) (zs ++ [x+y])
 
-main = putStrLn $ show $ sum (filter even (fib 1 2 [1,2]))
+main = putStrLn $ show $ sum $ filter even $ fib 1 2 [1,2]
 
