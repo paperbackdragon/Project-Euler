e97fc71ed2be4b2aaf39fe826019aabadf914b00
paperbd <heather.hb@gmail.com>
2012-06-10 08:49:28 +0100

    Solved problem 4 in Haskell.

--- haskell/prob4.hs
+++ haskell/prob4.hs
@@ -1,5 +1,29 @@
-{- A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91  99.
+{- A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 x 99.
 
  Find the largest palindrome made from the product of two 3-digit numbers. -}
 
+import Data.Char
+
+--Print answer
+main = putStrLn $ show $ maxPal
+
+--Find largest palindrome
+maxPal :: Integer
+maxPal = maximum [x | x <- palProducts threeDigit, isPalindrome x]
+
+--Figure out if a number is a palindrome
+isPalindrome :: Integer -> Bool
+isPalindrome x = (numToList x) == (reverse $ numToList x)
+
+--Find products of all combinations of two 3-digit numbers
+palProducts :: [Integer] -> [Integer]
+palProducts x = concatMap (\y -> map (y*) x) x
+
+--Convert a number into a list of its digits
+numToList :: Integer -> [Int]
+numToList x = map digitToInt (show x)
+
+--All 3-digit numbers
+threeDigit :: [Integer]
+threeDigit = [100..999]
 
